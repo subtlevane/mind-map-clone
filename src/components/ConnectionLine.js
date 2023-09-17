dbfb872381a33b74b700eded21f7d4557900201e
subtlevane <92869503+subtlevane@users.noreply.github.com>
2023-09-17 08:55:43 -0400

    Update ConnectionLine.js

--- src/components/ConnectionLine.js
+++ src/components/ConnectionLine.js
@@ -1,11 +1,10 @@
 export default function ConnectionLine({ from, to }) {
-    return (
-      <line 
-        x1={from.x} y1={from.y}
-        x2={to.x} y2={to.y}
-        stroke="#333"
-        strokeWidth="2"
-      />
-    );
-  }
-  
+  return (
+    <line 
+      x1={from.x} y1={from.y}
+      x2={to.x} y2={to.y}
+      stroke="#333"
+      strokeWidth="2"
+    />
+  );
+}
