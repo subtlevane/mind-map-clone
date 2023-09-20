99fd21e77de5cadbda113010a49d0150e34691c0
subtlevane <92869503+subtlevane@users.noreply.github.com>
2023-09-20 03:31:19 -0400

    1

--- src/components/ConnectionLine.js
+++ src/components/ConnectionLine.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import { useNodes } from './NodeContext'; // Adjust the path based on your project structure
+
+// Sample representation of all nodes in the mind map.
+// This should ideally come from a parent component or context.
+const allNodes = [
+  { id: 'node1', x: 100, y: 100 },
+  { id: 'node2', x: 300, y: 300 },
+  // ... other nodes
+];
 
 function checkForObstaclesBetween(from, to, nodes) {
   // Define a threshold to determine if a node is considered an "obstacle".
+  // This can be adjusted based on your requirements.
   const threshold = 20;
 
   for (let node of nodes) {
+    // Skip the 'from' and 'to' nodes.
     if (node === from || node === to) continue;
+
+    // Check if the node's x-coordinate is between the 'from' and 'to' x-coordinates.
     if (node.x > Math.min(from.x, to.x) + threshold && node.x < Math.max(from.x, to.x) - threshold) {
+      // Check if the node's y-coordinate is between the 'from' and 'to' y-coordinates.
       if (node.y > Math.min(from.y, to.y) + threshold && node.y < Math.max(from.y, to.y) - threshold) {
-        return true;
+        return true; // There's an obstacle between 'from' and 'to'.
       }
     }
   }
-  return false;
+
+  return false; // No obstacles found.
 }
 
 export default function ConnectionLine({ from, to }) {
-  const { nodes } = useNodes(); // Access the current node positions using the useNodes hook
   const nodeWidth = 100;
   const nodeHeight = 50;
+
+  let pathData;
 
   function refineControlPoints(from, to, obstacle) {
     const midpoint = {
@@ -54,11 +68,14 @@ export default function ConnectionLine({ from, to }) {
 
   function calculateBezier(from, to, obstacle) {
     const [controlPoint1, controlPoint2] = refineControlPoints(from, to, obstacle);
+
     return `M ${from.x + nodeWidth} ${from.y + nodeHeight / 2} C ${controlPoint1.x} ${controlPoint1.y}, ${controlPoint2.x} ${controlPoint2.y}, ${to.x} ${to.y + nodeHeight / 2}`;
   }
 
-  const obstacle = checkForObstaclesBetween(from, to, nodes);
-  const pathData = calculateBezier(from, to, obstacle);
+  // Check for obstacles between from and to
+  const obstacle = checkForObstaclesBetween(from, to, allNodes);
+
+  pathData = calculateBezier(from, to, obstacle);
 
   return (
     <svg style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', pointerEvents: 'none' }}>
--- src/styles/MindMapStyles.js
+++ src/styles/MindMapStyles.js
@@ -4,8 +4,10 @@ export const MindMapContainer = styled.div`
   position: relative;
   width: 100vw;
   height: 100vh;
-  background-color: #f0f0f0;
+  background-color: #ffffff;
+  border: 2px solid #d1d1d1; // Increased border thickness for better definition.
   overflow: hidden;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1); // Enhanced shadow for depth.
 `;
 
 export const ButtonContainer = styled.div`
@@ -17,19 +19,22 @@ export const ButtonContainer = styled.div`
 `;
 
 export const ActionButton = styled.button`
-  padding: 10px 20px;
+  padding: 12px 24px; // Increased padding for better touch targets.
   background-color: #007BFF;
   border: none;
-  border-radius: 5px;
+  border-radius: 6px; // Rounded corners for a modern touch.
   color: #ffffff;
   cursor: pointer;
-  transition: all 0.2s ease;
+  transition: all 0.3s ease-in-out; // Smoothed transitions.
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
 
   &:hover {
     background-color: #0056b3;
+    transform: translateY(-2px); // Lift effect on hover.
+    box-shadow: 0 4px 10px rgba(0, 123, 255, 0.2);
   }
 
   &:active {
-    transform: scale(0.95);
+    transform: scale(0.98); // Subtle press effect.
   }
 `;
